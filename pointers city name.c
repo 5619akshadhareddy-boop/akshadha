8237478ec63a7b66ce6767b87c533a2133e50f4f
5619akshadhareddy-boop <5619akshadhareddy@gmail.com>
2025-12-10 15:50:13 +0530

    Add files via upload

---
+++ pointers city name.c
@@ -0,0 +1,14 @@
+//reading and printing strings using string specific function.
+#include<stdio.h>
+int main(void)
+{
+	char str[20];
+	puts("Enter your city name:");
+    gets(str);      	//scanf("%s", str);
+	puts("your city Name:");
+	puts(str);
+}
+
+
+
+
